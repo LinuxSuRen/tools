dc6a74e0826aac83f5af783ded524e6278d4bb33
Rick <1450685+LinuxSuRen@users.noreply.github.com>
2022-05-25 22:31:51 +0800

    Support to install hd via wget or curl

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+which curl
+if [ $0 = 0 ]; then
 curl -L https://github.com/linuxsuren/http-downloader/releases/latest/download/hd-linux-amd64.tar.gz | tar xzv hd
+else
+wget https://github.com/linuxsuren/http-downloader/releases/latest/download/hd-linux-amd64.tar.gz -O - | tar xzv hd
+fi
+
 mv hd /usr/bin/hd
 hd fetch
 hd completion
