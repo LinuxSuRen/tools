62e36b1f39d7aa6a31a618ea417486b0172b883c
rick <1450685+LinuxSuRen@users.noreply.github.com>
2022-05-25 16:31:00 +0800

    Allow to pass flags to change templates

--- cmd/main.go
+++ cmd/main.go
@@ -10,54 +10,68 @@ import (
 	"path/filepath"
 )
 
-func main() {
-	cmd := cobra.Command{
-		Use: "yaml-readme",
-		RunE: func(cmd *cobra.Command, args []string) (err error) {
-			var items []map[string]string
+type option struct {
+	pattern      string
+	templateFile string
+}
 
-			// find YAML files
-			var files []string
-			var data []byte
-			if files, err = filepath.Glob("items/*.yaml"); err == nil {
-				for _, metaFile := range files {
-					if data, err = ioutil.ReadFile(metaFile); err != nil {
-						fmt.Println("failed to read file", metaFile, err)
-						continue
-					}
+func (o *option) runE(cmd *cobra.Command, args []string) (err error) {
+	var items []map[string]string
 
-					metaMap := make(map[string]string)
-					if err = yaml.Unmarshal(data, metaMap); err != nil {
-						fmt.Printf("failed to parse file [%s] as a YAML, error: %v\n", metaFile, err)
-						continue
-					}
-					items = append(items, metaMap)
-				}
+	// find YAML files
+	var files []string
+	var data []byte
+	if files, err = filepath.Glob(o.pattern); err == nil {
+		for _, metaFile := range files {
+			fmt.Println(metaFile)
+			if data, err = ioutil.ReadFile(metaFile); err != nil {
+				fmt.Println("failed to read file", metaFile, err)
+				continue
 			}
 
-			// load readme template
-			var readmeTpl string
-			if data, err = ioutil.ReadFile("README.tpl"); err != nil {
-				fmt.Printf("failed to load README template, error: %v\n", err)
-				readmeTpl = `
+			metaMap := make(map[string]string)
+			if err = yaml.Unmarshal(data, metaMap); err != nil {
+				fmt.Printf("failed to parse file [%s] as a YAML, error: %v\n", metaFile, err)
+				continue
+			}
+			items = append(items, metaMap)
+		}
+	}
+
+	// load readme template
+	var readmeTpl string
+	if data, err = ioutil.ReadFile(o.templateFile); err != nil {
+		fmt.Printf("failed to load README template, error: %v\n", err)
+		readmeTpl = `
 |中文名称|英文名称|JD|
 |---|---|---|
 {{- range $val := .}}
 |{{$val.zh}}|{{$val.en}}|{{$val.jd}}|
 {{end}}
 `
-			}
-			readmeTpl = string(data)
-
-			// generate readme file
-			var tpl *template.Template
-			if tpl, err = template.New("readme").Parse(readmeTpl); err != nil {
-				return
-			}
-			err = tpl.Execute(os.Stdout, items)
-			return
-		},
 	}
+	readmeTpl = string(data)
+
+	// generate readme file
+	var tpl *template.Template
+	if tpl, err = template.New("readme").Parse(readmeTpl); err != nil {
+		return
+	}
+	err = tpl.Execute(os.Stdout, items)
+	return
+}
+
+func main() {
+	opt := &option{}
+	cmd := cobra.Command{
+		Use:  "yaml-readme",
+		RunE: opt.runE,
+	}
+	flags := cmd.Flags()
+	flags.StringVarP(&opt.pattern, "pattern", "p", "items/*.yaml",
+		"The glob pattern with Golang spec to find files")
+	flags.StringVarP(&opt.templateFile, "template", "t", "README.tpl",
+		"The template file which should follow Golang template spec")
 	err := cmd.Execute()
 	if err != nil {
 		panic(err)
