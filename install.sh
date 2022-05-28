bf66276e2f53f4b3927b18e5667fcfe1783f29f8
Rick <1450685+LinuxSuRen@users.noreply.github.com>
2022-05-28 09:22:35 +0800

    Support to install hd in armv6

--- install.sh
+++ install.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
+ARCH=$(uname -m)
+if [[ ${ARCH} == 'x86_64' ]]; then
+  ARCH=amd64
+elif [[ ${ARCH} == 'armv6l' ]]
+  ARCH=arm6
+else
+  echo "do not support this arch"
+  echo "please feel free to help improve this at https://github.com/LinuxSuRen/tools/blob/master/install.sh"
+  exit 1
+fi
+
 which curl
 if [ $0 = 0 ]; then
-curl -L https://github.com/linuxsuren/http-downloader/releases/latest/download/hd-linux-amd64.tar.gz | tar xzv hd
+  curl -L https://github.com/linuxsuren/http-downloader/releases/latest/download/hd-linux-${ARCH}.tar.gz | tar xzv hd
 else
-wget https://github.com/linuxsuren/http-downloader/releases/latest/download/hd-linux-amd64.tar.gz -O - | tar xzv hd
+  wget https://github.com/linuxsuren/http-downloader/releases/latest/download/hd-linux-${ARCH}.tar.gz -O - | tar xzv hd
 fi
 
 mv hd /usr/bin/hd
