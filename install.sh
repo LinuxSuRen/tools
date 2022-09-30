57b086ec8eb3d260d536cc1ef18cc38d7ec460bc
Rick <1450685+LinuxSuRen@users.noreply.github.com>
2022-09-30 09:17:07 +0800

    Read the OS name from environment variables

--- install.sh
+++ install.sh
@@ -13,11 +13,22 @@ else
   exit 1
 fi
 
+OS=$(uname -s)
+if [ ${OS} == 'Darwin' ]; then
+  OS=darwin
+elif [ ${OS} == 'Linux' ]; then
+  OS=linux
+else
+  echo "do not support ${OS}"
+  echo "please feel free to help improve this at https://github.com/LinuxSuRen/tools/blob/master/install.sh"
+  exit 2
+fi
+
 which curl
 if [ $? = 0 ]; then
-  curl -L https://github.com/linuxsuren/http-downloader/releases/latest/download/hd-linux-${ARCH}.tar.gz | tar xzv hd
+  curl -L https://github.com/linuxsuren/http-downloader/releases/latest/download/hd-${OS}-${ARCH}.tar.gz | tar xzv hd
 else
-  wget https://github.com/linuxsuren/http-downloader/releases/latest/download/hd-linux-${ARCH}.tar.gz -O - | tar xzv hd
+  wget https://github.com/linuxsuren/http-downloader/releases/latest/download/hd-${OS}-${ARCH}.tar.gz -O - | tar xzv hd
 fi
 
 mv hd /usr/bin/hd
