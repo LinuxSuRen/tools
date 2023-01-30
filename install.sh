17e680f8104c02665b03618c91017a1919016a21
Rick <1450685+LinuxSuRen@users.noreply.github.com>
2023-01-30 15:43:25 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,10 +32,10 @@ select lng in github ghproxy
 do
     case $lng in
         "github")
-           address=https://github.com/linuxsuren/http-downloader/releases/latest/download/hd-${OS}-${ARCH}.tar.gz;;
+           address=https://github.com/linuxsuren/http-downloader/releases/latest/download/hd-${OS}-${ARCH}.tar.gz
            break;;
         "ghproxy")
-           address=https://ghproxy.com/https://github.com/linuxsuren/http-downloader/releases/latest/download/hd-${OS}-${ARCH}.tar.gz;;
+           address=https://ghproxy.com/https://github.com/linuxsuren/http-downloader/releases/latest/download/hd-${OS}-${ARCH}.tar.gz
            break;;
         *)
            echo "Ooops";;
